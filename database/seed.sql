a23521d1a5b4e0257d7b974fa35cce628d073a12
johnpaul5sing@gmail.com <johnpaul5sing@gmail.com>
2026-08-07 14:20:32 +0800

    feat: Added examples in seed.sql

---
+++ database/seed.sql
@@ -0,0 +1,44 @@
+INSERT INTO products
+(
+    product_name,
+    product_description,
+    product_price,
+    image_url,
+    category
+)
+VALUES
+(
+    'Classic Burger',
+    '100% Beef Patty',
+    120.00,
+    '/images/burger.jpg',
+    'Burger'
+),
+(
+    'Cheese Burger',
+    'Beef with Cheese',
+    150.00,
+    '/images/cheese-burger.jpg',
+    'Burger'
+),
+(
+    'French Fries',
+    'Crispy Fries',
+    80.00,
+    '/images/fries.jpg',
+    'Sides'
+),
+(
+    'Coke',
+    'Soft Drink',
+    50.00,
+    '/images/coke.jpg',
+    'Drinks'
+),
+(
+    'Iced Tea',
+    'Refreshing Tea',
+    60.00,
+    '/images/iced-tea.jpg',
+    'Drinks'
+);
